35b33181226af29f121a0f6e025bfa1b2f178e46
nyamzy <john.nyamweya@student.moringaschool.com>
2022-02-04 21:04:48 +0300

    create the view function for the home page

--- pix/views.py
+++ pix/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    title = "Pixies"
+    context = {
+        "title": title,
+    }
+    return render(request, "all-pix/index.html", context)
