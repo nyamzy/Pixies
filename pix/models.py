678b2c7956c0c9248a15a5687bdf3d8cc0f0388e
nyamzy <john.nyamweya@student.moringaschool.com>
2022-02-08 23:32:14 +0300

    create the models for our app

--- pix/models.py
+++ pix/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.forms import model_to_dict
 
 
 # Create your models here.
 class Profile(models.Model):
     profile_pic = models.ImageField(upload_to = 'images/')
     bio = models.CharField(max_length=100)
-    user = models.OneToOneField(User, on_delete = models.CASCADE, null = True)
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
 
     def __str__(self):
         return self.bio
@@ -16,29 +15,32 @@ class Profile(models.Model):
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
     image_name = models.CharField(max_length=30)
-    caption = models.CharField(max_length=100)
+    image_caption = models.CharField(max_length=100)
     pub_date = models.DateTimeField(auto_now_add = True)
+    like_count = models.ManyToManyField(User, default = 0, related_name='liked')
+    comment_count = models.IntegerField(default = 0)
     profile = models.ForeignKey(User, on_delete = models.CASCADE, null = True)
-    likes = models.ManyToManyField(User, related_name = 'liked', default = 0, null=True)
-    comments = models.IntegerField(default = 0)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = image, null = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, related_name = 'user')
 
     def __str__(self):
         return self.image_name
 
 
 class Like(models.Model):
-    LIKE_CHOICES = {
+    LIKE_CHOICES = [
         ('Like', 'Like'),
         ('Unlike', 'Unlike'),
-    }
+    ]
 
     image = models.ForeignKey(Image, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    value = models.CharField(choices=LIKE_CHOICES, default='like', max_length=10)
+    value = models.CharField(choices=LIKE_CHOICES, default='Like', max_length=10)
+
+    def str(self):
+        return self.value
 
 
 class Comment(models.Model):
     comment = models.CharField(max_length = 200)
-    image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name='comments')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'comments')
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
