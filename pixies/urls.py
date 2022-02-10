e16467d21107f04ebe38cd353b7ec80909ee2ce4
nyamzy <john.nyamweya@student.moringaschool.com>
2022-02-10 20:41:08 +0300

    include django registration urls

--- pixies/urls.py
+++ pixies/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pix.urls'),)
+    path('', include('pix.urls'),),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
