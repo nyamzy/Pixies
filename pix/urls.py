4c1989e16812a5a4f2b1e03a69e7775485ca65a5
nyamzy <john.nyamweya@student.moringaschool.com>
2022-02-11 16:28:17 +0300

    add new url configuraion

--- pix/urls.py
+++ pix/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name = "home"),
     path('search/', views.search_results, name = 'search_results'),
+    path('new/post/', views.new_post, name = "new-post"),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
